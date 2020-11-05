35dea8a6c3cb67b2389f84bf98a1745b4845dc80
Jadin Press <jadinp@gmail.com>
2020-11-05 07:20:34 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,31 @@
-# Write your methods here
+require 'pry'
+
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  index = 0
+  while index < array.length do
+    puts array[index]
+    index += 1
+  end
+end
+
+def return_string_array(array)
+  string_array = Array.new
+  index = 0
+  while index < array.length do
+    string_array << array[index].to_s
+    index += 1
+  end
+  string_array
+end
